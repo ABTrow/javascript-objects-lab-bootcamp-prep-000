e0dd74d4249635b9452b311685c3f0c22790f6b2
Austin Trow <austintrow@gmail.com>
2019-06-19 18:29:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var recipes = {eggs: "2 large", flour: "3 cups", butter: "4 pats"}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign(object, {[key]: value});
+  return Object.assign({}, object, {[key]: value});
 }
