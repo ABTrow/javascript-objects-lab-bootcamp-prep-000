076cf94898cfeab8bf66c6659c10e9ae363a6af5
Austin Trow <austintrow@gmail.com>
2019-06-19 18:36:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,4 +15,7 @@ function deleteFromObjectByKey(object, key) {
   return copy;
 }
 
-function 
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object;
+}
